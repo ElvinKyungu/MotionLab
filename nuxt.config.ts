7efa7bab4791bfebeaa3215a83698284ceb58645
Elvinkyungu <elvinkyungu.75@gmail.com>
2026-04-14 09:18:29 +0100

    feat: configure Roboto font and update Nuxt configuration for improved styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,25 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+        }
+      ]
+    }
+  },
   modules: [
     '@nuxt/a11y',
     '@nuxt/eslint',
